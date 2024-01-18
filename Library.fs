3ba87705025ac1f12ae5a6b820e4c7f9d0f28cf0
Marat Gevorkyan <marat2255a@gmail.com>
2024-01-18 05:59:41 +0300

    add runBot function with updates handlers feature

--- Library.fs
+++ Library.fs
@@ -1,2 +1,73 @@
-﻿namespace Telefunc
+﻿module Telefunc
 
+open Telegram.Bot
+open Telegram.Bot.Types
+open System.Threading
+open Telegram.Bot.Polling
+open System.Threading.Tasks
+open Telegram.Bot.Types.Enums
+open Telegram.Bot.Exceptions
+open System
+
+type UpdateHandler = delegate of ITelegramBotClient * Update -> bool
+
+let inline private awaitTask (task: Task) =
+    task |> Async.AwaitTask |> Async.RunSynchronously
+
+let inline private awaitTypedTask task =
+    task |> Async.AwaitTask |> Async.RunSynchronously
+
+let inline private onEnterLine fn =
+    Console.ReadLine() |> ignore
+    fn ()
+
+let inline newBot (token: string) = new TelegramBotClient(token)
+
+let inline private updatesChecker
+    (handlers: UpdateHandler list)
+    (update: Update)
+    (bot: ITelegramBotClient)
+    =
+    handlers
+    |> Seq.tryFind (fun handler -> handler.Invoke(bot, update))
+    |> Option.isSome
+
+let inline private handleUpdates
+    (updatesChecker)
+    (bot: ITelegramBotClient)
+    (update: Update)
+    (_)
+    =
+    task {
+        let gotUpdate = updatesChecker update bot
+
+        if gotUpdate then ()
+        else
+            raise (new ApiRequestException("Didn't match any branch."))
+    }
+    :> Task
+
+let inline runBot
+    (token: string)
+    (updates: UpdateHandler list)
+    (excHandler: ITelegramBotClient -> exn -> CancellationToken -> Task)
+    =
+    use cts = new CancellationTokenSource()
+
+    let receiverOptions = new ReceiverOptions()
+    receiverOptions.AllowedUpdates <- Array.empty<UpdateType>
+
+    let bot = newBot token
+
+    bot.StartReceiving(
+        updateHandler       = (handleUpdates <| updatesChecker updates),
+        pollingErrorHandler = excHandler,
+        receiverOptions     = receiverOptions,
+        cancellationToken   = cts.Token
+    )
+
+    let me = bot.GetMeAsync() |> awaitTypedTask
+
+    printfn "Start listening for %s (%d)" me.Username me.Id
+
+    onEnterLine cts.Cancel
